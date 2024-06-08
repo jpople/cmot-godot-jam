5ce4209197e206a19d74d3eb120071db739e9fda
Cawotte <khamaz.ee@gmail.com>
2024-06-08 19:29:58 -0400

    Can kinda add building to the  grid
    A bit barebone for now

--- Scripts/Gameboard.cs
+++ Scripts/Gameboard.cs
@@ -1,29 +1,72 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+
+public struct Cell 
+{
+	public Vector2I cellPos;
+	public Vector2 localCenter;
+	public BuildingData data;
+
+	public Cell( Vector2I cellPos, Vector2 localCenter, BuildingData data )
+	{
+		this.cellPos = cellPos;
+		this.localCenter = localCenter;
+		this.data = data;
+	}
+}
 
 public partial class Gameboard : Node2D
 {
-	
 	[Export] public TileMap tilemap;
 
 	[Signal] public delegate void GridMouseMotionEventHandler( bool hasChangedCell, Vector2I cellPos, Vector2 globalPos );
+	[Signal] public delegate void GridMouseButtonEventHandler( Vector2I cellPos, Vector2 globalPos );
 
 	public Vector2 HoveredCell { get; private set; }
 
+	public Dictionary<Vector2I, Cell> Grid { get; private set; } = new Dictionary<Vector2I, Cell>();
 
 	// Return the center local pos of the cell the mouse is currently hovering on
 	public Vector2 GetMouseCurrentCellCenter()
 	{
 		Vector2 localMousePos = this.GetLocalMousePosition();
-		Vector2I mapPosition = tilemap.LocalToMap( localMousePos );
-		return tilemap.MapToLocal( mapPosition );
+		Vector2I cellPos = tilemap.LocalToMap( localMousePos );
+		return tilemap.MapToLocal( cellPos );
 	}
-
 
 	// Return the cell coordinate at the given position
 	public Vector2I LocalToCellPos( Vector2 localPosition )
 	{
 		return tilemap.LocalToMap( localPosition );
+	}
+
+	public bool HasBuilding( Vector2I cellPos )
+	{
+		if ( !Grid.ContainsKey( cellPos ))
+		{
+			return false;
+		}
+
+		Cell cell = Grid[ cellPos ];
+
+		return cell.data != null;
+	}
+
+	public bool AddBuilding( Vector2I cellPos, BuildingData buildingData )
+	{
+		if ( HasBuilding( cellPos ) )
+			return false;
+
+		Cell newCell = new Cell( cellPos, tilemap.MapToLocal( cellPos ), buildingData );
+		Grid[ cellPos ] = newCell;
+
+		// TODO : Get Sprite and Instantiate SpriteNode? Setting them directly into tilemap seems clunky.
+		// Placeholder : place a generic red tile
+		Vector2I redTileAtlasPos = new Vector2I( 0, 6 );
+		tilemap.SetCell( 0, cellPos, 1, redTileAtlasPos );
+
+		return true;
 	}
 
 /**
@@ -33,20 +76,28 @@ public partial class Gameboard : Node2D
 	private void OnArea2DEntered( Viewport viewport, InputEvent inputEvent, int shapeIdx )
 	{
 		InputEventMouseMotion mouseEvent = inputEvent as InputEventMouseMotion;
-
-		if ( mouseEvent == null )
-			return;
+		InputEventMouseButton buttonEvent = inputEvent as InputEventMouseButton;
 
 		Vector2 localMousePos 		= this.GetLocalMousePosition();
 		Vector2I newHoveredCellPos 	= this.LocalToCellPos( localMousePos );
 		Vector2 cellCenterPos 		= GetMouseCurrentCellCenter();
 		bool hasChangedCell 		= HoveredCell == newHoveredCellPos;
 
-		HoveredCell = newHoveredCellPos;
+		if ( buttonEvent != null )
+		{
+			//Add a fake building for now
+			BuildingData data = new BuildingData();
+			AddBuilding( newHoveredCellPos, data );
 
-		EmitSignal(SignalName.GridMouseMotion, hasChangedCell, newHoveredCellPos, cellCenterPos );
+			EmitSignal( SignalName.GridMouseButton, newHoveredCellPos, cellCenterPos );
+		}
 
-		return;
+		if ( mouseEvent != null )
+		{
+			HoveredCell = newHoveredCellPos;
+
+			EmitSignal(SignalName.GridMouseMotion, hasChangedCell, newHoveredCellPos, cellCenterPos );
+		}
 	}
 
 }
