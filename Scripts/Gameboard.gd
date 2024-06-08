5d296a8bcc02151b363bd637f66fe66e40e635e9
Cawotte <khamaz.ee@gmail.com>
2024-06-08 11:51:14 -0400

    Isometric Grid
    and cursor does follow it correctly

--- Scripts/Gameboard.gd
+++ Scripts/Gameboard.gd
@@ -7,6 +7,8 @@ extends Node2D
 # Should probably be moved in its own class
 @export var cursor : Node2D
 
+@export var tilemap : TileMap
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -23,11 +25,9 @@ func _on_area_2d_input_event(viewport, event, shape_idx):
 	if !mouseEvent:
 		return
 		
-	var globalMousePos = self.get_global_mouse_position();
 	var localMousePos = self.get_local_mouse_position();
-	var localPosition = self.to_local(event.position)
+	var mapPosition = tilemap.local_to_map ( localMousePos )
+	var tileCenter = tilemap.map_to_local( mapPosition )
 	
-	var cellCenter = grid.local_pos_to_grid_center_pos( globalMousePos )
-	
-	cursor.global_position = cellCenter
+	cursor.position = tileCenter
 
